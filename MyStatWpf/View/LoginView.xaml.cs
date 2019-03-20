a581f407d344f82e0e26b0dceb0f0cda134fbfa9
Valeriy Kovetskiy <kovetskiy.valeriy@gmail.com>
2019-03-20 20:54:36 +0200

    Caliburn integration

--- MyStatWpf/View/LoginView.xaml.cs
+++ MyStatWpf/View/LoginView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MyStatWpf
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для LoginView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginView : Window
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
         }
